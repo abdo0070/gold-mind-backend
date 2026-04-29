d825ae04b9d696c51580d15393c5dec23675ce76
unknown <abdallammdouh6@gmail.com>
2026-04-29 14:59:14 +0300

    add jwt auth

--- Program.cs
+++ Program.cs
@@ -16,6 +16,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var jwtOptions = new JwtOptions();
+
+builder.Configuration.GetSection("JWT").Bind(jwtOptions);
+builder.Services.AddSingleton(jwtOptions);
+
 builder.Services.AddDbContext<AppDBContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DB_URI")));
 
 builder.Services.AddCors(opt => opt.AddDefaultPolicy(
@@ -23,7 +28,14 @@ policy =>
 policy.AllowAnyHeader()
 .AllowAnyOrigin()
 .AllowAnyMethod()
-)); 
+));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(jwtOpt =>
+    {
+        jwtOpt.Audience = jwtOpt.Audience;
+        jwtOpt.Authority = jwtOpt.Authority;
+    });
 
 var app = builder.Build();
 
